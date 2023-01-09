fa3174065814fb183ec815996990ec5d5d9d583d
Manshi <98829866+Manshi34-github@users.noreply.github.com>
2023-01-09 20:17:14 +0530

    Update FamilyData.js

--- src/Data/FamilyData.js
+++ src/Data/FamilyData.js
@@ -1,27 +1,55 @@
+import aditya from "../Images/aditya.JPG";
+import shyam from "../Images/shyam.jpg";
+import manshi from "../Images/manshi.jpg";
+import pawan from "../Images/pawan.jpg";
+import abhijeet from "../Images/abhijeet.jpeg";
+import ayush from "../Images/ayush.jpg";
+import harsh  from "../Images/harsh.jpg";
+import geetika from "../Images/geetika.jpg";
+import mandal from "../Images/mandal.jpg";
+import rajesh from "../Images/rajesh.jpg";
+import suresh from "../Images/suresh.jpg";
+import manish from "../Images/manish.jpeg";
+
+
 const Data = [
   {
     id: 1,
-    year: "Fourth",
+    year: "Faculty Members",
     teams: [
       {
         teammates: [
           {
-            studentPic:
-              "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80",
-            studentName: "Aditya Garg",
-            position: "President",
-            email: "sahupawan9749568594@gmail.com",
+            studentPic:mandal,
+            studentName: "Dr. Sasankasekhar Mandal",
+            position: "Head of Department",
+            email: "smandal.civ@iitbhu.ac.in",
             linkedIn: "https://www.linkedin.com/in/pawan-kumar-sahu-4329a2208/",
-            contactNo: "9749568594",
+            contactNo: "9415256211",
           },
           {
-            studentPic:
-              "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80",
-            studentName: "Rakshit Gautam",
-            position: "Vice President",
-            email: "sahupawan9749568594@gmail.com",
+            studentPic:mandal,
+              studentName:"Dr. Sasankasekhar Mandal",
+            position: "President",
+            email: "smandal.civ@iitbhu.ac.in",
             linkedIn: "https://www.linkedin.com/in/pawan-kumar-sahu-4329a2208/",
-            contactNo: "9749568594",
+            contactNo: "9415256211",
+          },
+          {
+            studentPic:suresh, 
+            studentName: "Dr. Suresh Kumar",
+            position: "Treasurer",
+            email: "skumar.civ@iitbhu.ac.in",
+            linkedIn: "https://www.linkedin.com/in/pawan-kumar-sahu-4329a2208/",
+            contactNo: " 8800157987",
+          },
+          {
+            studentPic:rajesh,
+            studentName: "Rajesh Kumar",
+            position: "Secretary",
+            email: " rkumar.civ@iitbhu.ac.in",
+            linkedIn: "https://www.linkedin.com/in/pawan-kumar-sahu-4329a2208/",
+            contactNo: "9453788219",
           },
         ],
       },
@@ -29,166 +57,96 @@ const Data = [
   },
   {
     id: 2,
-    year: "Third",
+    year: "Student Members",
     teams: [
       {
         teamname: "Responsibility Head",
         teammates: [
           {
+            studentPic:"https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80",
+            studentName: "Rakshit Goutam",
+            position: "General Secretary",
+            email: "rakshit.goutam.civ19@itbhu.ac.in",
+            linkedIn: "https://www.linkedin.com/in/rakshit-goutam-239272194/",
+            contactNo: "9119148281",
+          },
+          {
             studentPic:
-              "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80",
+              aditya,
+            studentName: "Aditya Garg",
+            position: "Vice President",
+            email: "aditya.garg.civ19@itbhu.ac.in",
+            linkedIn: "https://www.linkedin.com/in/aditya-garg-190/",
+            contactNo: "7088335377",
+          },
+          {
+            studentPic:manish,            
             studentName: "Manish Saini",
             position: "Joint Secretary",
-            email: "sahupawan9749568594@gmail.com",
-            linkedIn: "https://www.linkedin.com/in/pawan-kumar-sahu-4329a2208/",
-            contactNo: "9749568594",
+            email: "mainsh.saini.civ20@itbhu.ac.in",
+            linkedIn: "https://www.linkedin.com/in/manish-saini-9ba601208/",
+            contactNo: "8824214834",
           },
           {
-            studentPic:
-              "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80",
+            studentPic:geetika,
             studentName: "Geetika Singh",
             position: "Joint Secretary",
-            email: "sahupawan9749568594@gmail.com",
-            linkedIn: "https://www.linkedin.com/in/pawan-kumar-sahu-4329a2208/",
-            contactNo: "9749568594",
+            email: "geetika.singh.civ20@itbhu.ac.in",
+            linkedIn: "https://www.linkedin.com/in/geetika-singh-b80602208/",
+            contactNo: "8574211708",
           },
+          {
+            studentPic:shyam,
+            studentName: "Shyam Sundar Singh",
+            position: "Public Relations Head",
+            email: "shyam.sundersingh.cd.civ20@itbhu.ac.in",
+            linkedIn: "https://www.linkedin.com/in/shyamsunder01/",
+            contactNo: "9671287331",
+          },
+          {
+            studentPic:ayush,            
+            studentName: "Ayush Sinha",
+            position: "Alumni Realtions Head",
+            email: "ayush sinha.civ20@itbhu.ac.in",
+            linkedIn: "https://www.linkedin.com/in/pawan-kumar-sahu-4329a2208/",
+            contactNo: "8677921299",
+          },
+          {
+            studentPic:manshi,
+            studentName: "Manshi Kumari",
+            position: "Tech Head",
+            email: "manshi.kumari.civ20@itbhu.ac.in",
+            linkedIn: "https://www.linkedin.com/in/manshi-kumari-83b373221/",
+            contactNo: "9693075960",
+          },
+          {
+            studentPic:pawan,
+            studentName: "Pawan Kumar Sahu",
+            position: "Events & Operational Head",
+            email: "pawan.kumarsahu.civ20@itbhu.ac.in",
+            linkedIn: "https://www.linkedin.com/in/pawan-kumar-sahu-4329a2208/",
+            contactNo: "9749365897",
+          },
+          {
+            studentPic:abhijeet,
+            studentName: "Abhijeet Kumar Gaurav",
+            position: "Content & Social Media Head",
+            email: "abhijeet.kumargaurav.civ20@itbhu.ac.in",
+            linkedIn: "https://www.linkedin.com/in/abhijeet-kumar-gaurav-133b2321a/",
+            contactNo: "7764026333",
+          },
+          {
+            studentPic:harsh,            
+            studentName: "Harsh Vardhan Badaya",
+            position: "Finance Head",
+            email: "harsh.singhbadaya.cd.civ20@itbhu.ac.in",
+            linkedIn: "https://www.linkedin.com/in/harsh-vardhan-badaya-619349201/",
+            contactNo: "8209180221",
+          },
+         
         ],
       },
-      {
-        teamname: "Tech Team",
-        teammates: [
-          {
-            studentPic:
-              "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80",
-            studentName: "Pawan Kumar Sahu",
-            place: "Upper",
-            position: "Team Advisor",
-            email: "sahupawan9749568594@gmail.com",
-            linkedIn: "https://www.linkedin.com/in/pawan-kumar-sahu-4329a2208/",
-            contactNo: "9749568594",
-          },
-          {
-            studentPic:
-              "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80",
-            studentName: "Pawan Kumar Sahu",
-            position: "Team Head",
-            place: "Upper",
-            email: "sahupawan9749568594@gmail.com",
-            linkedIn: "https://www.linkedin.com/in/pawan-kumar-sahu-4329a2208/",
-            contactNo: "9749568594",
-          },
-          {
-            studentPic:
-              "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80",
-            studentName: "Pawan Kumar Sahu",
-            position: "Executive",
-            place: "Lower",
-            email: "sahupawan9749568594@gmail.com",
-            linkedIn: "https://www.linkedin.com/in/pawan-kumar-sahu-4329a2208/",
-            contactNo: "9749568594",
-          },
-          {
-            studentPic:
-              "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80",
-            studentName: "Pawan Kumar Sahu",
-            position: "Executive",
-            place: "Lower",
-            email: "sahupawan9749568594@gmail.com",
-            linkedIn: "https://www.linkedin.com/in/pawan-kumar-sahu-4329a2208/",
-            contactNo: "9749568594",
-          },
-          {
-            studentPic:
-              "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80",
-            studentName: "Pawan Kumar Sahu",
-            position: "Executive",
-            place: "Lower",
-            email: "sahupawan9749568594@gmail.com",
-            linkedIn: "https://www.linkedin.com/in/pawan-kumar-sahu-4329a2208/",
-            contactNo: "9749568594",
-          },
-        ],
-      },
-      {
-        teamname: "Publicity Team",
-        teammates: [
-          {
-            studentPic:
-              "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80",
-            studentName: "Pawan Kumar Sahu",
-            position: "Team Head",
-            place: "Upper",
-            email: "sahupawan9749568594@gmail.com",
-            linkedIn: "https://www.linkedin.com/in/pawan-kumar-sahu-4329a2208/",
-            contactNo: "9749568594",
-          },
-          {
-            studentPic:
-              "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80",
-            studentName: "Pawan Kumar Sahu",
-            position: "Executive",
-            place: "Lower",
-            email: "sahupawan9749568594@gmail.com",
-            linkedIn: "https://www.linkedin.com/in/pawan-kumar-sahu-4329a2208/",
-            contactNo: "9749568594",
-          },
-          {
-            studentPic:
-              "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80",
-            studentName: "Pawan Kumar Sahu",
-            position: "Executive",
-            place: "Lower",
-            email: "sahupawan9749568594@gmail.com",
-            linkedIn: "https://www.linkedin.com/in/pawan-kumar-sahu-4329a2208/",
-            contactNo: "9749568594",
-          },
-          {
-            studentPic:
-              "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80",
-            studentName: "Pawan Kumar Sahu",
-            position: "Executive",
-            place: "Lower",
-            email: "sahupawan9749568594@gmail.com",
-            linkedIn: "https://www.linkedin.com/in/pawan-kumar-sahu-4329a2208/",
-            contactNo: "9749568594",
-          },
-        ],
-      },
-      {
-        teamname: "Alumni and Relation Team",
-        teammates: [
-          {
-            studentPic:
-              "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80",
-            studentName: "Pawan Kumar Sahu",
-            position: "Team Head",
-            place: "Upper",
-            email: "sahupawan9749568594@gmail.com",
-            linkedIn: "https://www.linkedin.com/in/pawan-kumar-sahu-4329a2208/",
-            contactNo: "9749568594",
-          },
-          {
-            studentPic:
-              "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80",
-            studentName: "Pawan Kumar Sahu",
-            position: "Executive",
-            place: "Lower",
-            email: "sahupawan9749568594@gmail.com",
-            linkedIn: "https://www.linkedin.com/in/pawan-kumar-sahu-4329a2208/",
-            contactNo: "9749568594",
-          },
-          {
-            studentPic:
-              "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80",
-            studentName: "Pawan Kumar Sahu",
-            position: "Executive",
-            place: "Lower",
-            email: "sahupawan9749568594@gmail.com",
-            linkedIn: "https://www.linkedin.com/in/pawan-kumar-sahu-4329a2208/",
-            contactNo: "9749568594",
-          },
-        ],
-      },
+
     ],
   },
 ];
