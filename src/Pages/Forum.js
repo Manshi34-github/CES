62f0dd3e21ae014fc279d04ccb0bef0c225560ed
Indresh <indresh.lucky101@gmail.com>
2023-01-12 23:25:36 +0530

    Image gallery and forum css updated

--- src/Pages/Forum.js
+++ src/Pages/Forum.js
@@ -9,72 +9,73 @@ const Forum = () => {
     let edit_forum = document.getElementById('edit-forum');
     let filterValue = edit_forum.value;
 
-    if(filterValue!=='All'){
+    if (filterValue !== 'All') {
       for (let index = 0; index < Data.length; index++) {
-        let titles = document.getElementById(index+1);
+        let titles = document.getElementById(index + 1);
         if (Data[index].forum !== filterValue) {
-          ForumArray.push(index+1);
+          ForumArray.push(index + 1);
           titles.style.display = 'none';
         }
-      } 
-    }  
+      }
+    }
   }
 
-  const HandleStyleProperty = ()=>{
+  const HandleStyleProperty = () => {
     for (let index = 0; index < ForumArray.length; index++) {
       let titles = document.getElementById(ForumArray[index]);
       titles.removeAttribute('style');
 
     }
-    
+
   }
-
-
   return (
     <div>
       <Navbar />
       <div className="pt-20">
-        <div className="ml-12 pt-4 flex justify-center">
-          <h2 className="underline underline-offset-8 m-1 text-2xl mb-3 ">Forum Topics</h2>
-          <select id="edit-forum" name="forum" className="form-select border-2 rounded outline hover:outline-0 m-1 px-5" onChange={HandleStyleProperty}>
+        <div className="pt-1 text-right w-4/5 ml-9">
+    
+          <span><select id="edit-forum" name="forum" className="form-select mt-1 rounded outline hover:outline-0 m-0.5 px-0.5" onChange={HandleStyleProperty}>
             <option value="All" defaultValue="Any">- Any -</option>
             <option value="Student-Activities">Student-Activities</option>
             <option value="Courses">Courses</option>
             <option value="Information">Information</option>
           </select>
-          <button type="submit" className="m-1 border-2 border-black px-4 bg-gray-200 font-bold" onClick={HandleApply}>Apply</button>
+          </span>
+          <span><button type="submit" className="transition ease-in delay-150 border-2 border-blue-800 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-400 duration-100 ml-3 px-4 rounded font-bold" onClick={HandleApply}>Apply</button></span>
         </div>
-        <div className="ml-7 p-7 flex justify-center">
-          <table className="table-auto p-10 w-5/6 border-2">
+
+      
+        <div className=" p-3 flex justify-center">
+          <table className="table-auto p-10 shadow-2xl w-2/3">
             <thead>
-              <tr className=" border-2">
+              <tr className=" border-2 bg-gradient-to-r from-blue-400 to-stone-200  mt-2 border-zinc-400">
                 <th>Topic</th>
                 <th>Replies</th>
                 <th>Date</th>
                 <th>Forum</th>
               </tr>
             </thead>
-            <tbody>
+            <tbody className="border-2 border-zinc-400 bg-stone-100">
               {Data.map((post) => {
                 return (
-                  <>
-                    <tr id={post.id} >
-                      <td>
-                        <Link className="text-sky-500" to={"/forum/" + post.id}>{post.title}</Link>
-                        <br />
-                        <span className="text-xs">Autonomus on Sunday 15:45 PM {post.date}</span>
-                      </td>
-                      <td>50+</td>
-                      <td>{post.date}</td>
-                      <td id="forum">{post.forum}</td>
-                    </tr>
 
-                  </>
+                  <tr id={post.id} key={post.id} >
+                    <td>
+                      <Link className="text-sky-500 m-2 mt-3 hover:text-lg hover:bg-blue-200 hover:text-black hover:border rounded" key={post.id} to={"/forum/"+post.id}>{post.title}</Link>
+                      <br />
+                      <span className="text-xs m-2 mt-3">Autonomus on Sunday 15:45 PM {post.date}</span>
+                    </td>
+                    <td>50+</td>
+                    <td>{post.date}</td>
+                    <td id="forum">{post.forum}</td>
+                  </tr>
                 );
               })}
 
             </tbody>
           </table>
+
+
         </div>
       </div>
       <Footer />
