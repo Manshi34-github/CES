5f7b012bd319b4501f6dd068d7eba85879ce4fe8
Manshi <98829866+Manshi34-github@users.noreply.github.com>
2023-02-01 18:48:56 +0530

    Update ContactData.js

--- src/Data/ContactData.js
+++ src/Data/ContactData.js
@@ -1,4 +1,17 @@
-import Tanay from "../Images/Antriksh.jpg";
+
+import palak from "../Images/palak.jfif";
+import vaibhav from "../Images/vaibhav.jfif";
+import preethi from "../Images/preethi.jfif";
+import yatnesh from "../Images/yatnesh.jfif";
+import palash from "../Images/palash.jfif";
+import aman from "../Images/aman.jfif";
+import prashant from "../Images/prashant.jfif";
+import rohan from "../Images/rohan.jfif";
+import arushikha from "../Images/arushikha.jfif";
+import anmol from "../Images/anmol.jfif";
+import deepali from "../Images/deepali.jfif";
+import prachi from "../Images/prachi.jfif";
+import apoorva from "../Images/apoorva.jpg";
 
 const Data = [
   {
@@ -6,11 +19,11 @@ const Data = [
     topic: "SDE",
     student: [
       {
-        picture: Tanay,
-        name: "Pawan Kumar Sahu",
-        year: "2020",
-        email: "sahupawan9749568594@gmail.com",
-        linkedIn: "https://www.linkedin.com/in/pawan-kumar-sahu-4329a2208/",
+        picture: palak,
+        name: "Palak Gupta",
+        year: "2018",
+        email: "palak.gupta.civ18@itbhu.ac.in",
+        linkedIn: "https://www.linkedin.com/in/palak-gupta-5438a2171/",
         contact: "9749568594",
         facebook: "https://www.facebook.com/pawankumar.sahu.7127146",
         
@@ -18,31 +31,31 @@ const Data = [
       
       
       {
-        picture: Tanay,
-        name: "Pawan Kumar Sahu",
-        year: "2020",
-        email: "sahupawan9749568594@gmail.com",
-        linkedIn: "https://www.linkedin.com/in/pawan-kumar-sahu-4329a2208/",
+        picture:vaibhav,
+        name: "Vaibhav Shukla",
+        year: "2015",
+        email: "vaibhav.sukla.civ18@itbhu.ac.in",
+        linkedIn: "https://www.linkedin.com/in/vaibhav-shukla-742a97195/",
         contact: "9749568594",
         facebook: "https://www.facebook.com/pawankumar.sahu.7127146",
         
       },
       {
-        picture: Tanay,
-        name: "Pawan Kumar Sahu",
-        year: "2020",
-        email: "sahupawan9749568594@gmail.com",
-        linkedIn: "https://www.linkedin.com/in/pawan-kumar-sahu-4329a2208/",
+        picture: preethi,
+        name: "Preethi Bodapati",
+        year: "2015",
+        email: "preethi.bodapati.civ15@itbhu.ac.in",
+        linkedIn: "https://www.linkedin.com/in/preethi-bodapati-579839122/",
         contact: "9749568594",
         facebook: "https://www.facebook.com/pawankumar.sahu.7127146",
         
       },
       {
-        picture: Tanay,
-        name: "Pawan Kumar Sahu",
-        year: "2020",
-        email: "sahupawan9749568594@gmail.com",
-        linkedIn: "https://www.linkedin.com/in/pawan-kumar-sahu-4329a2208/",
+        picture: yatnesh,
+        name:"Yatnesh Gyani",
+        year: "2018",
+        email: "yatnesh.gyani.civ18@itbhu.ac.in",
+        linkedIn: "https://www.linkedin.com/in/yatnesh-gyani-6271381b2/",
         contact: "9749568594",
         facebook: "https://www.facebook.com/pawankumar.sahu.7127146",
         
@@ -54,31 +67,31 @@ const Data = [
     topic: "Gate",
     student: [
       {
-        picture: Tanay,
-        name: "Pawan Kumar Sahu",
-        year: "2020",
-        email: "sahupawan9749568594@gmail.com",
-        linkedIn: "https://www.linkedin.com/in/pawan-kumar-sahu-4329a2208/",
+        picture: palash,
+        name: "Palash Pagaria",
+        year: "2011",
+        email: "palash.pagaria.civ11@itbhu.ac.in",
+        linkedIn: "https://www.linkedin.com/in/palash-pagaria-jain-7681826b//",
         contact: "9749568594",
         facebook: "https://www.facebook.com/pawankumar.sahu.7127146",
         
       },
       {
-        picture: Tanay,
-        name: "Pawan Kumar Sahu",
-        year: "2020",
-        email: "sahupawan9749568594@gmail.com",
-        linkedIn: "https://www.linkedin.com/in/pawan-kumar-sahu-4329a2208/",
+        picture: aman,
+        name: "Aman Gupta",
+        year: "2011",
+        email: "aman.gupta.civ11@itbhu.ac.in",
+        linkedIn: "https://www.linkedin.com/in/aman-gupta-4a462463/",
         contact: "9749568594",
         facebook: "https://www.facebook.com/pawankumar.sahu.7127146",
         
       },
       {
-        picture: Tanay,
-        name: "Pawan Kumar Sahu",
-        year: "2020",
-        email: "sahupawan9749568594@gmail.com",
-        linkedIn: "https://www.linkedin.com/in/pawan-kumar-sahu-4329a2208/",
+        picture: prashant,
+        name: "Prashant Singh",
+        year: "2015",
+        email: "prashant.singh.civ15@itbhu.ac.in",
+        linkedIn: "https://www.linkedin.com/in/prashant-singh-15b385137/",
         contact: "9749568594",
         facebook: "https://www.facebook.com/pawankumar.sahu.7127146",
         
@@ -90,35 +103,73 @@ const Data = [
     topic: "Analyst",
     student: [
       {
-        picture: Tanay,
-        name: "Pawan Kumar Sahu",
-        year: "2020",
-        email: "sahupawan9749568594@gmail.com",
-        linkedIn: "https://www.linkedin.com/in/pawan-kumar-sahu-4329a2208/",
+        picture:rohan,
+        name: "Rohan Nathiley",
+        year: "2016",
+        email: "rohan.nathiley.civ16@itbhu.ac.in",
+        linkedIn: "https://www.linkedin.com/in/rohan-nathiley-155a21150/",
         contact: "9749568594",
         facebook: "https://www.facebook.com/pawankumar.sahu.7127146",
         
       },
       {
-        picture: Tanay,
-        name: "Pawan Kumar Sahu",
-        year: "2020",
-        email: "sahupawan9749568594@gmail.com",
-        linkedIn: "https://www.linkedin.com/in/pawan-kumar-sahu-4329a2208/",
+        picture: arushikha,
+        name: "Arushikha Tayal",
+        year: "2018",
+        email: "arushikha.tayal.civ18@itbhu.ac.in",
+        linkedIn: "https://www.linkedin.com/in/arushikha-tayal-8b18b6194/",
         contact: "9749568594",
         facebook: "https://www.facebook.com/pawankumar.sahu.7127146",
         
       },
       {
-        picture: Tanay,
-        name: "Pawan Kumar Sahu",
-        year: "2020",
-        email: "sahupawan9749568594@gmail.com",
-        linkedIn: "https://www.linkedin.com/in/pawan-kumar-sahu-4329a2208/",
+        picture:anmol,
+        name: "Anmol Saluja",
+        year: "2018",
+        email: "anmol.saluja.civ18@itbhu.ac.in",
+        linkedIn: "https://www.linkedin.com/in/anmol-saluja-106085170/",
         contact: "9749568594",
         facebook: "https://www.facebook.com/pawankumar.sahu.7127146",
         
       },
+     
+    ],
+  },
+  {
+    id: 4,
+    topic: "Product Management",
+    student: [
+      {
+        picture: deepali,
+        name: "Deepali Saxena",
+        year: "2005",
+        email: "deepali.saxena.civ05@itbhu.ac.in",
+        linkedIn: "https://www.linkedin.com/in/deepali-saxena-9b916020/",
+        contact: "9749568594",
+        facebook: "https://www.facebook.com/pawankumar.sahu.7127146",
+        
+      },
+      {
+        picture: prachi,
+        name: "Prachi Mahajan",
+        year: "2009",
+        email: "prachi.mahajan.civ09@itbhu.ac.in",
+        linkedIn: "https://www.linkedin.com/in/prachimahajan01/",
+        contact: "9749568594",
+        facebook: "https://www.facebook.com/pawankumar.sahu.7127146",
+        
+      },
+      {
+        picture: apoorva,
+        name: "Apoorva Kumar",
+        year: "2016",
+        email: "apoorva.kumar.civ16@itbhu.ac.in",
+        linkedIn: "https://www.linkedin.com/in/apoorvkumar988/",
+        contact: "9749568594",
+        facebook: "https://www.facebook.com/pawankumar.sahu.7127146",
+        
+      },
+     
     ],
   },
 ];
