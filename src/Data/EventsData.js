59e62b3e08803cb67d33192238ca3b6e56b5ddb0
Singh Ritiwij Birbahadur <ritiwijsingh2003@gmail.com>
2023-02-01 18:23:59 +0530

    Image added in event page

--- src/Data/EventsData.js
+++ src/Data/EventsData.js
@@ -1,11 +1,13 @@
-import Event from "../Images/event.jpg";
-
+import Event1 from '../Images/event1.jpg'
+import Event2 from '../Images/event2.jpg'
+import Event3 from '../Images/event3.jpg'
+import Event4 from '../Images/event4.jpg'
 
 
 const Data = [
   {
     id: 1,
-    image: Event,
+    image: Event1,
     topic: "Lorem Ipsum",
     content:
       "lorem50fkdfgndlfndfdfogmdfjbbmlbjdfpmdfjdfjdf kldjgdogjdoghflmff d idgodigpdfgofig fpgifggiopsgipgur rg s Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
@@ -17,7 +19,7 @@ const Data = [
   },
   {
     id: 2,
-    image: Event,
+    image: Event2,
     topic: "Lorem Ipsum",
     content:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
@@ -29,7 +31,7 @@ const Data = [
   },
   {
     id: 3,
-    image: Event,
+    image: Event3,
     topic: "Lorem Ipsum",
     content:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
@@ -41,7 +43,7 @@ const Data = [
   },
   {
     id: 4,
-    image: Event,
+    image: Event4,
     topic: "Lorem Ipsum",
     content:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
@@ -51,30 +53,7 @@ const Data = [
     place: "flex-row",
     color: "bg-red-400",
   },
-  {
-    id: 5,
-    image: Event,
-    topic: "Lorem Ipsum",
-    content:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-    location: "G2",
-    time: "5:45 pm",
-    date: "22/07/22",
-    place: "flex-row",
-    color: "bg-green-400",
-  },
-  {
-    id: 6,
-    image: Event,
-    topic: "Lorem Ipsum",
-    content:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-    location: "G2",
-    time: "5:45 pm",
-    date: "22/07/22",
-    place: "flex-row",
-    color: "bg-red-400",
-  },
+
 ];
 
 
